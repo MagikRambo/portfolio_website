b071d5c525d4475129083a6567df6db5b721bd3d
MIKE MARAMBIO <u1229804@umail.utah.edu>
2022-01-09 18:18:39 -0700

    removing unnecessary files and updating tank image

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,45 +1,58 @@
 export const projects = [
   {
-    title: 'U-Study App',
-    description: "Using HTML, CSS, JavaScript & Node. You'll be able to block out any distractions to help study during COVID times. A Chrome extension built to help blacklist websites that are non-educational, with the ability to whitelist pages.",
-    image: '/images/1.png',
-    tags: ['HTML5', 'CSS3', 'JavaScript', 'node'],
-    source: 'https://devpost.com/software/ustudyapp',
-    visit: 'https://github.com/Mikemarambio/LateEntryHack',
+    title: "U-Study App",
+    description:
+      "Using HTML, CSS, JavaScript & Node. You'll be able to block out any distractions to help study during COVID times. A Chrome extension built to help blacklist websites that are non-educational, with the ability to whitelist pages.",
+    image: "/images/1.png",
+    tags: ["HTML5", "CSS3", "JavaScript", "node"],
+    source: "https://devpost.com/software/ustudyapp",
+    visit: "https://github.com/Mikemarambio/LateEntryHack",
     id: 0,
   },
   {
-    title: 'Personal Portfolio',
-    description:"Using REACT, JavaScript, This is a website to showcase my projects and a little bit about myself. Also my first time building a website using a framework.",
-    image: '/images/2.png',
-    tags: ['React', 'JavaScript', 'Vercel'],
-    source: 'https://github.com/Mikemarambio/portfolio_website',
-    visit: 'https://github.com/Mikemarambio/portfolio_website',
+    title: "Personal Portfolio",
+    description:
+      "Using REACT, JavaScript, This is a website to showcase my projects and a little bit about myself. Also my first time building a website using a framework.",
+    image: "/images/2.png",
+    tags: ["React", "JavaScript", "Vercel"],
+    source: "https://github.com/Mikemarambio/portfolio_website",
+    visit: "https://github.com/Mikemarambio/portfolio_website",
     id: 1,
   },
   {
-    title: 'Multi-user TCP-IP/IP Spreadsheet',
-    description: "This is a similar and more relaxed version of Excel. This project utilized multi-client threading in C++ for the backend. Along with a GUI made in C#.",
-      image: '/images/3.png',
-      tags: ['C++', 'C#', 'Docker'],
-    source: 'https://github.com/Mikemarambio/R4M',
-    visit: 'https://github.com/Mikemarambio/R4M',
+    title: "Multi-user TCP-IP/IP Spreadsheet",
+    description:
+      "This is a similar and more relaxed version of Excel. This project utilized multi-client threading in C++ for the backend. Along with a GUI made in C#.",
+    image: "/images/3.png",
+    tags: ["C++", "C#", "Docker"],
+    source: "https://github.com/Mikemarambio/R4M",
+    visit: "https://github.com/Mikemarambio/R4M",
     id: 2,
   },
   {
-    title: 'TankWars',
-    description: "This is a game created in C# utilizing TCP/IP connection to a server with multiple people to play against eachother. Utilizing ray tracing.",
-    image: '/images/4.png',
-    tags: ['C#'],
-    source: 'https://github.com/uofu-cs3500-fall20/game-team_power_move',
-    visit: 'https://github.com/uofu-cs3500-fall20/game-team_power_move',
+    title: "TankWars",
+    description:
+      "This is a game created in C# utilizing TCP/IP connection to a server with multiple people to play against eachother. Utilizing ray tracing.",
+    image: "/images/four.png",
+    tags: ["C#"],
+    source: "https://github.com/uofu-cs3500-fall20/game-team_power_move",
+    visit: "https://github.com/uofu-cs3500-fall20/game-team_power_move",
     id: 3,
   },
 ];
 
 export const TimeLineData = [
-  { year: 2019, text: 'Started my journey at the University of Utah', },
-  { year: 2020, text: 'Won the Best Beginner hack award at the HackTheU2020 Hackathon', },
-  { year: 2021, text: 'Achieved my first internship with Microsoft in the summer', },
-  { year: 2022, text: 'Built more projects and returning to Microsoft for the second year', },
+  { year: 2019, text: "Started my journey at the University of Utah" },
+  {
+    year: 2020,
+    text: "Won the Best Beginner hack award at the HackTheU2020 Hackathon",
+  },
+  {
+    year: 2021,
+    text: "Achieved my first internship with Microsoft in the summer",
+  },
+  {
+    year: 2022,
+    text: "Built more projects and returning to Microsoft for the second year",
+  },
 ];
