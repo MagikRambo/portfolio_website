3541085e0cd9d168c3629e9af62023ae3b389a52
Mike Marambio <mikemarambio@Mikes-MacBook-Pro-2.local>
2022-10-14 01:58:49 -0600

    actually fixed hero format

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -10,14 +10,16 @@ const Hero = (props) => (
     <SectionTitle main center>
       <p>Mike Marambio</p>
     </SectionTitle>
+    <SectionText>
     <Img src = "/images/hero.png"/>
+    </SectionText>
     <SectionText>  
       <br/>
       <p>Welcome to my site! <br/> <br/>
       My name is Mike Marambio. I'm currently studying Computer Science at the University of Utah, doing Research in the Visual Perception and Spatial Cognition lab under Professor Jeanine Stefanucci on topics of Virtual Reality and Mixed Reality with a focus on Mixed Reality.
       <br/>
       <br/>
-      <p>To learn more about me please scroll down! :D</p>
+      <p>To learn more about me scroll down! :D</p>
       
       </p>
     </SectionText>
