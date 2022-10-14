c00192f651028c54faca925d8df4f7246c4c9fce
Mike Marambio <mikemarambio@Mikes-MacBook-Pro-2.local>
2022-10-14 01:53:06 -0600

    minor updates for formatting of hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -10,15 +10,14 @@ const Hero = (props) => (
     <SectionTitle main center>
       <p>Mike Marambio</p>
     </SectionTitle>
-    <SectionText>
-      <Img src = "/images/hero.png"/>
-      
-      <p>Welcome to my site! <br/>
+    <Img src = "/images/hero.png"/>
+    <SectionText>  
+      <br/>
+      <p>Welcome to my site! <br/> <br/>
       My name is Mike Marambio. I'm currently studying Computer Science at the University of Utah, doing Research in the Visual Perception and Spatial Cognition lab under Professor Jeanine Stefanucci on topics of Virtual Reality and Mixed Reality with a focus on Mixed Reality.
       <br/>
       <br/>
-
-      To learn more about me please scroll down! :D
+      <p>To learn more about me please scroll down! :D</p>
       
       </p>
     </SectionText>
